8f2198ed77d5751732fd96b76f812d63b0638f78
carpfishz <lsml3541@gmail.com>
2016-11-28 15:35:35 +0900

    edit index.php, index.css and update indexbg.jpg

--- index.php
+++ index.php
@@ -13,17 +13,26 @@ if($login->is_loggedin()!="") {
 <head>
     <meta charset="UTF-8">
     <title>SBS Planner</title>
-    <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/init.css">
+    <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/nanumbarungothic.css">
     <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
     <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 </head>
 <body>
-<form action="login.php" method="POST" id="login_form">
-    <span>ID</span><input type="text" name="userEmail" placeholder="아이디 또는 이메일 주소" required>
-    <span>PW</span><input type="password" name="userPw" placeholder="비밀번호" required>
-    <input type="submit" value="로그인" name="submit" id="login_submit">
-</form>
+<div id="wrapper">
+    <div id="image-wrapper">
+        <h1>
+            <a href="/sbs">SBS Planner</a>
+        </h1>
+    </div>
+    <div id="form-wrapper">
+        <form action="login.php" method="POST" id="login_form">
+            <input type="text" name="userEmail" placeholder="아이디 또는 이메일 주소" required>
+            <input type="password" name="userPw" placeholder="비밀번호" required>
+            <input type="submit" value="로그인" name="submit" id="login_submit">
+        </form>
+    </div>
+</div>
 </body>
 </html>
